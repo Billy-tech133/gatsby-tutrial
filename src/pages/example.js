578f21664c7cd5ba60171be045168804426a6025
Nyuykisamo Lamfih <lamfihbilly@hotmail.com>
2021-09-21 15:52:48 +0100

    static query

--- src/pages/example.js
+++ src/pages/example.js
@@ -1,14 +1,38 @@
 import React from "react"
 import Header from "../examples/Header"
+import ComponentName from "../examples/HeaderStatic"
 import Layout from "../components/layout"
-import Navbar from "../components/Navbar"
-const example = () => {
+import { graphql } from "gatsby"
+const example = ({ data }) => {
+  const {
+    site: {
+      info: { author },
+    },
+  } = data
   return (
     <Layout>
       <h2>Hi from example page</h2>
       <Header />
+      <ComponentName />
+      <h5>author: {author}</h5>
     </Layout>
   )
 }
 
+export const data = graphql`
+  query MyQuery {
+    site {
+      info: siteMetadata {
+        author
+        data
+        description
+        person {
+          age
+          name
+        }
+        title
+      }
+    }
+  }
+`
 export default example
