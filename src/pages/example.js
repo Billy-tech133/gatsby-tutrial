ea61b08f4bcf4d73a6d8a4217b42113f97f52327
Nyuykisamo Lamfih <lamfihbilly@hotmail.com>
2021-09-21 11:05:38 +0100

    graphql-static-query

---
+++ src/pages/example.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "../examples/Header"
+import Layout from "../components/layout"
+import Navbar from "../components/Navbar"
+const example = () => {
+  return (
+    <Layout>
+      <h2>Hi from example page</h2>
+      <Header />
+    </Layout>
+  )
+}
+
+export default example
